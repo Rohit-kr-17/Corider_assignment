db607215a0c982b38aecf835340a161289aeb17d
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-10-24 15:17:38 +0530

    Attachment button ui

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,11 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
-import { BiArrowBack } from "react-icons/bi";
-import { LiaEditSolid } from "react-icons/lia";
-import { ImAttachment } from "react-icons/im";
-import { VscSend } from "react-icons/vsc";
+
 import { BiLoaderAlt } from "react-icons/bi";
-import { BsThreeDotsVertical } from "react-icons/bs";
 
 type ChatProps = {
 	setShowChat: React.Dispatch<React.SetStateAction<boolean>>;
@@ -38,85 +34,56 @@ type ChatData = {
 };
 
 export default function Chat(props: ChatProps) {
-	const [pageNo, setPageNo] = useState(0);
 	const [isLoadiing, setIsLoading] = useState(false);
-	const [autoScroll, setAutoScroll] = useState(true);
+	const [viewAttachment, setViewAttachment] = useState(false);
+	const [pageNo, setPageNo] = useState(0);
 	const chatContainerRef = useRef<HTMLDivElement | null>(null);
-
+	const [currScrollpos, setCurrScrollpos] = useState(0);
 	const handleClick = () => {
 		props.setShowChat(false);
 	};
-	const renderMessagesWithDate = () => {
-		let prevDate = "";
-		return props.chatData?.chats.map((chat) => {
-			const formattedDate = new Date(chat.time).toLocaleDateString();
-			if (prevDate === "") {
-				prevDate = formattedDate;
-			}
-			if (formattedDate !== prevDate) {
-				const temp = prevDate;
-				prevDate = formattedDate;
-				return (
-					<div key={chat.id}>
-						{<DateComponent date={temp} />}
-						<Message chat={chat} />
-					</div>
-				);
-			}
-		});
+	const handleAttachment = () => {
+		setViewAttachment(!viewAttachment);
 	};
+	const handleScroll = () => {
+		if (chatContainerRef.current) {
+			if (chatContainerRef.current.scrollTop === 0) {
+				setIsLoading(true);
+				setPageNo(pageNo + 1);
+
+				getMoreChats();
+				chatContainerRef.current.scrollTop = currScrollpos;
+			} else setIsLoading(false);
+		}
+	};
+
+	const getMoreChats = async () => {
+		const data = await axios.get(
+			`https://qa.corider.in/assignment/chat?page=${pageNo}`
+		);
+		if (data) {
+			setIsLoading(false);
+			if (props.chatData)
+				props.chatData.chats = [...data.data.chats, ...props.chatData.chats];
+		}
+	};
+
+	useEffect(() => {
+		if (chatContainerRef.current)
+			chatContainerRef.current.scrollTop =
+				chatContainerRef.current?.scrollHeight;
+	}, []);
+
 	const Loader = () => {
 		if (isLoadiing)
 			return (
-				<div className="text-2xl flex justify-center ">
+				<div className="text-2xl h-5 flex justify-center ">
 					<BiLoaderAlt className="transition animate-spin" />
 				</div>
 			);
 		else return <></>;
 	};
 
-	const handleScroll = () => {
-		if (chatContainerRef.current) {
-			//console.log(chatContainerRef.current.scrollTop);
-			const isAtBottom =
-				chatContainerRef.current.clientHeight -
-					chatContainerRef.current.scrollHeight ===
-				Math.round(chatContainerRef.current.scrollTop);
-
-			if (isAtBottom) {
-				setAutoScroll(false);
-			} else {
-				setAutoScroll(true);
-			}
-
-			if (autoScroll && isAtBottom) {
-				getMoreChatData();
-			}
-		}
-	};
-
-	const getMoreChatData = async () => {
-		try {
-			setIsLoading(true);
-			const chat: AxiosResponse<ChatData> = await axios.get(
-				`https://qa.corider.in/assignment/chat?page=${pageNo + 1}`
-			);
-			if (chat.data.status === "success") {
-				const mapReverse1 = chat.data.chats
-					.slice(0)
-					.reverse()
-					.map((element) => {
-						return element;
-					});
-				setPageNo(pageNo + 1);
-				if (props.chatData)
-					props.chatData.chats = [...mapReverse1, ...props.chatData.chats];
-				setIsLoading(false);
-			}
-		} catch (error) {
-			console.error("Error fetching more chat data:", error);
-		}
-	};
 	return (
 		<div
 			className="h-screen bg-[#FAF9F4] flex flex-col"
@@ -236,19 +203,18 @@ export default function Chat(props: ChatProps) {
 					</div>
 				</div>
 			</div>
+			{isLoadiing && Loader()}
 			<div
 				ref={chatContainerRef}
 				onScroll={handleScroll}
-				className="bg-[#FAF9F4] flex-1 flex flex-col-reverse overflow-y-auto"
+				className="bg-[#FAF9F4] flex-1  overflow-y-auto"
 			>
-				<div className=" p-4">{renderMessagesWithDate()}</div>
-
 				<div className=" p-4">
 					{props.chatData?.chats &&
 						props.chatData?.chats.map((chat) => <Message chat={chat} />)}
 				</div>
-				<div className="bg-[#FAF9F4] p-4">{Loader()}</div>
 			</div>
+
 			<div className="flex items-center justify-center bg-[#FAF9F4] z-10 shadow-top p-4 h-20">
 				<div className="flex items-center h-[42px] justify-between w-full pr-4 bg-[white] rounded-[8px]">
 					<input
@@ -256,23 +222,32 @@ export default function Chat(props: ChatProps) {
 						className="w-full pl-4 rounded-[8px] h-[42px] outline-none"
 						placeholder="Enter your text"
 					/>
+					{/* <Attachment /> */}
 					<div className="flex items-center justify-between">
-						<svg
-							className="mr-4"
-							width="20"
-							height="20"
-							viewBox="0 0 20 20"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M17.6271 9.08291L10.1141 16.5959C8.40554 18.3045 5.63544 18.3045 3.9269 16.5959C2.21835 14.8874 2.21835 12.1173 3.9269 10.4087L11.4399 2.89573C12.5789 1.7567 14.4257 1.7567 15.5647 2.89573C16.7037 4.03476 16.7037 5.88149 15.5647 7.02052L8.34631 14.2389C7.7768 14.8084 6.85343 14.8084 6.28392 14.2389C5.7144 13.6694 5.7144 12.746 6.28392 12.1765L12.6184 5.84201"
-								stroke="#141E0D"
-								strokeWidth="1.5"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-							/>
-						</svg>
+						<div className="flex relative flex-col justify-center items-center">
+							{viewAttachment && (
+								<div className="absolute translate-y-[-45px] translate-x-[-7px] flex flex-col items-center justify-center rounded-full p-1 cursor-pointer">
+									<Attachment />
+								</div>
+							)}
+							<svg
+								onClick={handleAttachment}
+								className="mr-4"
+								width="20"
+								height="20"
+								viewBox="0 0 20 20"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M17.6271 9.08291L10.1141 16.5959C8.40554 18.3045 5.63544 18.3045 3.9269 16.5959C2.21835 14.8874 2.21835 12.1173 3.9269 10.4087L11.4399 2.89573C12.5789 1.7567 14.4257 1.7567 15.5647 2.89573C16.7037 4.03476 16.7037 5.88149 15.5647 7.02052L8.34631 14.2389C7.7768 14.8084 6.85343 14.8084 6.28392 14.2389C5.7144 13.6694 5.7144 12.746 6.28392 12.1765L12.6184 5.84201"
+									stroke="#141E0D"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+							</svg>
+						</div>
 
 						<svg
 							width="18"
@@ -324,6 +299,72 @@ export const Message = (props: MessageProps) => {
 		);
 	}
 };
-const DateComponent = ({ date }: { date: string }) => (
-	<div className="text-center text-gray-500 py-2">{date}</div>
-);
+
+export const Attachment = () => {
+	return (
+		<>
+			<div className="h-[44px] pt-[12px] pr-[16px] pb-[12px] pl-[16px] items-center w-[124px] border-1 border-[#008000] bg-[#008000] rounded-full flex justify-between">
+				<svg
+					width="20"
+					height="20"
+					viewBox="0 0 20 20"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M1.66667 6.41814C1.66667 5.17468 2.67469 4.16666 3.91815 4.16666C4.56422 4.16666 5.13781 3.75324 5.34211 3.14032L5.41667 2.91666C5.45183 2.81118 5.46941 2.75844 5.48821 2.71166C5.72837 2.11426 6.29116 1.70863 6.93385 1.6697C6.98418 1.66666 7.03977 1.66666 7.15095 1.66666H12.8491C12.9602 1.66666 13.0158 1.66666 13.0662 1.6697C13.7088 1.70863 14.2716 2.11426 14.5118 2.71166C14.5306 2.75844 14.5482 2.81118 14.5833 2.91666L14.6579 3.14032C14.8622 3.75324 15.4358 4.16666 16.0819 4.16666C17.3253 4.16666 18.3333 5.17468 18.3333 6.41814V13.5C18.3333 14.9001 18.3333 15.6002 18.0608 16.135C17.8212 16.6054 17.4387 16.9878 16.9683 17.2275C16.4335 17.5 15.7335 17.5 14.3333 17.5H5.66667C4.26654 17.5 3.56647 17.5 3.03169 17.2275C2.56129 16.9878 2.17883 16.6054 1.93915 16.135C1.66667 15.6002 1.66667 14.9001 1.66667 13.5V6.41814Z"
+						stroke="white"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M10 13.75C12.0711 13.75 13.75 12.0711 13.75 9.99999C13.75 7.92892 12.0711 6.24999 10 6.24999C7.92893 6.24999 6.25 7.92892 6.25 9.99999C6.25 12.0711 7.92893 13.75 10 13.75Z"
+						stroke="white"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+				<svg
+					width="20"
+					height="20"
+					viewBox="0 0 20 20"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M18 6.42857L12.9091 10L18 13.5714V6.42857Z"
+						stroke="white"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M11.4545 5H3.45455C2.65122 5 2 5.63959 2 6.42857V13.5714C2 14.3604 2.65122 15 3.45455 15H11.4545C12.2579 15 12.9091 14.3604 12.9091 13.5714V6.42857C12.9091 5.63959 12.2579 5 11.4545 5Z"
+						stroke="white"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+				<svg
+					width="20"
+					height="20"
+					viewBox="0 0 20 20"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M16.6667 10.4167V5.66666C16.6667 4.26653 16.6667 3.56646 16.3942 3.03168C16.1545 2.56128 15.7721 2.17882 15.3016 1.93914C14.7669 1.66666 14.0668 1.66666 12.6667 1.66666H7.33334C5.9332 1.66666 5.23314 1.66666 4.69836 1.93914C4.22795 2.17882 3.8455 2.56128 3.60582 3.03168C3.33334 3.56646 3.33334 4.26653 3.33334 5.66666V14.3333C3.33334 15.7335 3.33334 16.4335 3.60582 16.9683C3.8455 17.4387 4.22795 17.8212 4.69836 18.0608C5.23314 18.3333 5.93317 18.3333 7.33322 18.3333H10.4167M11.6667 9.16666H6.66667M8.33334 12.5H6.66667M13.3333 5.83332H6.66667M12.5 15.8333L15 18.3333M15 18.3333L17.5 15.8333M15 18.3333V13.3333"
+						stroke="white"
+						stroke-width="1.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</div>
+			<div className="w-[16px] h-[8px] border-1 rotate-45 translate-y-[-6px] border-[#008000] bg-[#008000]"></div>
+		</>
+	);
+};
